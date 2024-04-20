30492d25a189727c3920bb357d10717b93c6ee7a
Naglafarn <84658977+Naglafarn@users.noreply.github.com>
2024-04-20 12:36:58 +0000

    Setup for Test Server - Initial - 20.04.2024

---
+++ setuptestserver.bat
@@ -0,0 +1,14 @@
+winget.exe install --id WinSCP.WinSCP --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id WinDirStat.WinDirStat --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --name "Windows Terminal Preview" --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.TCPView --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.Sysmon --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.ProcessMonitor --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.ProcessExplorer --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.Autoruns --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.BGInfo --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id ShareX.ShareX --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id PuTTY.PuTTY --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Notepad++.Notepad++ --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.AzureCLI --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id 7zip.7zip --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
