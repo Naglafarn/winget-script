3af1187d7caed75eece663e988cb669b0a04e0f5
Naglafarn <84658977+Naglafarn@users.noreply.github.com>
2024-04-20 12:22:03 +0000

    Initial Deployment - Only Irfanview

---
+++ setupdev.bat
@@ -0,0 +1,2 @@
+winget.exe install --id IrfanSkiljan.IrfanView --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id IrfanSkiljan.IrfanView.PlugIns --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
