be9f92a78ba5d5f08bc314637521221cba6440a3
Naglafarn <84658977+Naglafarn@users.noreply.github.com>
2024-04-20 12:30:54 +0000

    Update for Dev Client - 20.04.2024

--- setupdev.bat
+++ setupdev.bat
@@ -1,2 +1,20 @@
 winget.exe install --id IrfanSkiljan.IrfanView --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
 winget.exe install --id IrfanSkiljan.IrfanView.PlugIns --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id WinSCP.WinSCP --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id WinDirStat.WinDirStat --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id VideoLAN.VLC --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --name "Windows Terminal Preview" --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.TCPView --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.Sysmon --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.ProcessMonitor --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.ProcessExplorer --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.Autoruns --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.Sysinternals.BGInfo --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id ShareX.ShareX --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Rufus.Rufus --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id PuTTY.PuTTY --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Notepad++.Notepad++ --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id Microsoft.AzureCLI --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id JGraph.Draw --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id DeepL.DeepL --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
+winget.exe install --id 7zip.7zip --exact --accept-source-agreements --silent --disable-interactivity --accept-package-agreements
